b3dfc334da018c0fec52f2e3e455f2b361401097
Alex Chavez <104519597+alexcha2204@users.noreply.github.com>
2024-08-20 17:52:23 -0400

    Update releaseRadarClone.py
    remove azure instances and updated code

--- releaseRadarClone.py
+++ releaseRadarClone.py
@@ -4,14 +4,15 @@ from datetime import datetime
 from spotipy.oauth2 import SpotifyOAuth
 from spotipy.exceptions import SpotifyException
 from flask import Flask, request, url_for, session, redirect
-from azure.storage.blob import BlobServiceClient
 from collections import defaultdict
 import pandas as pd
 import json
 
 spotifyApp = Flask(__name__)
 spotifyApp.config['SESSION_COOKIE_NAME'] = 'Release Radar Cookie'
-spotifyApp.secret_key = 'YOUR_SECRET_KEY'
+spotifyApp.secret_key = 'ehfuhrv&@98#8b976fvt$52'
+DELAY_TIME = 2
+BATCH_SIZE = 50
 TOKEN_INFO = 'token_info'
 
 @spotifyApp.route('/')
@@ -33,58 +34,53 @@ def release_radar_clone():
     try:
         token_info = get_token()
     except Exception as e:
-        print(f"Error: {str(e)}")
+        print(f"Error obtaining Spotify token: {str(e)}")
         return redirect('/')
 
     sp = spotipy.Spotify(auth=token_info['access_token'])
-    user_songs = {}
-    delay_time = 2
-
-    batch_size = 50
+    artist_data = {}
+    songs = {}
     offset = 0
+
     while True:
         try:
-            liked_songs = sp.current_user_saved_tracks(limit=batch_size, offset=offset)['items']
+            liked_songs = sp.current_user_saved_tracks(limit=BATCH_SIZE, offset=offset)['items']
 
             for song in liked_songs:
                 local = song['track']['is_local']
                 if not local:
-                    get_song_data(user_songs,song)
+                    get_music_data(artist_data, song, songs)
 
-            if len(liked_songs) < batch_size:
+            if len(liked_songs) < BATCH_SIZE:
                 break
 
-            offset += batch_size
+            offset += BATCH_SIZE
 
         except SpotifyException as e:
             if e.http_status == 429:
-                retry_after = int(e.headers.get('Retry-After', delay_time))
+                retry_after = int(e.headers.get('Retry-After', DELAY_TIME))
                 time.sleep(retry_after)
             else:
-                print(f"Spotify API error: {e}")
+                print(f"Spotify API error getting user's saved tracks: {e}")
                 break
 
-    user_genres = get_genre_mapping(sp, user_songs)
+    genres_data = get_genre_mapping(sp, artist_data)
 
-    upload_music_data_locally(user_genres, user_songs)
+    upload_music_data_locally(sp, artist_data, genres_data, songs)
     
     return "IT WORKS"
 
+
 def get_genre_mapping(sp: spotipy.Spotify, user_data: dict):
 
-    genres_artists_pairs = defaultdict(list)
-    batch_size = 50 #max output for artists is 50 
-
-    # getting all artists ids based on the user_data dictionary passed through
+    genres_dict = defaultdict(list)
     artist_ids = [artist['artist_id'] for artist in user_data.values()]
     artist_ids_length = len(artist_ids)
 
-    # creating a new batch of ids for every 50 based on our ids list
-    for start in range (0, artist_ids_length, batch_size):
-       end = start + batch_size
+    for start in range (0, artist_ids_length, BATCH_SIZE):
+       end = start + BATCH_SIZE
        chunk_of_artist_ids = artist_ids[start:end]
 
-        # for every 50 artists, get their name and their associated genres and add it to the genre map
        artists_info = sp.artists(chunk_of_artist_ids)['artists']
 
        for artist_info in artists_info:
@@ -92,31 +88,124 @@ def get_genre_mapping(sp: spotipy.Spotify, user_data: dict):
            artist_genres = artist_info['genres']
 
            for genre in artist_genres:
-               genres_artists_pairs[genre].append(artist_name)
+               genres_dict[genre].append(artist_name)
 
-    return genres_artists_pairs
+    return genres_dict
 
-def upload_music_data_locally(sp: spotipy.Spotify, genres_dict: dict, artists_dict: dict):
-    
-    # converting dictionaries to json/dataframe 
+def upload_music_data_locally(sp: spotipy.Spotify, artists_dict: dict, genres_dict: dict, songs:dict):
 
     artist_df = pd.DataFrame.from_dict(artists_dict, orient='index')
    
     username = get_spotify_username(sp)
 
-    # save artists data to a csv file
     csv_filename = f"music_data_{username}.csv"
-    json_filename = f"genres_{username}.json"
+    genres_json_filename = f"genres_{username}.json"
+    # albums_json_filename = f"albums_{username}.json"
 
     artist_df.to_csv(csv_filename, index=False)
 
-    # Assuming 'genres_json' is your JSON data
-    with open(json_filename, 'w') as json_file:
+    with open(genres_json_filename, 'w') as json_file:
         json.dump(genres_dict, json_file, indent=4)
 
-def artists_data_dictionary(artist_name: str, artist_id: str, first_added: datetime, first_song: str, first_album: str, first_album_type: str,
-                              last_added: datetime, last_song: str, last_album: str, last_album_type: str, num_songs_main: int, num_songs_feature: int, liked_songs: int,
-                              genres_count: int):
+    with open("juice_wrld_saved.json", 'w') as json_file:
+        json.dump(songs, json_file, indent=4)
+
+    # with open(albums_json_filename, 'w') as json_file:
+    #     json.dump(albums_dict, json_file, indent=4)
+       
+
+def get_music_data(artists_data: dict, song: dict,  all_songs: dict):
+    
+    artists = song['track']['artists']
+    date_added = datetime.fromisoformat(song['added_at'])
+    song_name = song['track']['name']
+    match_name = "Juice WRLD"
+
+    if 'album' in song['track'] and song['track']['album']:
+        album_name = song['track']['album']['name']
+        album_type = song['track']['album']['album_type']
+    else:
+        album_name = None
+        album_type = None
+
+    if album_name is None:
+        print(f"Skipping song due to missing information: {song_name} by {artists}")
+    else:
+        for artist in artists:
+
+            artist_name = artist['name']
+            artist_id = artist['id']
+
+            if artist_name not in artists_data:
+
+                artists_data[artist_name] = artists_data_dictionary(artist_name, artist_id, 
+                                                                        date_added, song_name, album_name, album_type,
+                                                                        date_added, song_name, album_name, album_type, 
+                                                                        0, 0, 0, 0)
+                
+            if artist_name == artists[0]['name']:
+                artists_data[artist_name]["main_songs_count"] += 1
+            else:
+                artists_data[artist_name]["featured_songs_count"] += 1
+
+            # if artist_name not in all_songs and artist_name == match_name:
+            #     all_songs[artist_name] = {"id": artist_id, 'songs': {}}
+
+            #     album_details = {
+            #         "album_name": album_name,
+            #         "album_id": song['track']['album']['id'],
+            #         "release_date": song['track']['album']['release_date'],
+            #         'date_added': date_added.isoformat(),
+            #         "album_songs": []
+            #     }
+
+            #     all_songs[artist_name]['songs'][album_name] = album_details 
+            #     all_songs[artist_name]['songs'][album_name]['album_songs'].append({
+            #         "song_name": song_name,
+            #         'song_id': song['track']['id'],
+            #         "date_added": date_added.isoformat(),
+            #         "explicit": song['track']['explicit']
+            #     })
+
+            # if artist_name == match_name:
+            #     if album_name not in all_songs[artist_name]['songs']:
+            #         all_songs[artist_name]['songs'][album_name] = {
+            #             "release_date": song['track']['album']['release_date'],
+            #             "album_songs": []
+            #         }
+
+            #     if song['track']['album']['id'] not in all_songs[artist_name]['songs'][album_name]:
+            #         all_songs[artist_name]['songs'][album_name][song['track']['album']['id']] = {
+            #             "release_date": song['track']['album']['release_date'],
+            #             "album_songs": []
+            #         }
+
+            #     all_songs[artist_name]['songs'][album_name][song['track']['album']['id']]['album_songs'].append({
+            #         "song_name": song_name,
+            #         'song_id': song['track']['id'],
+            #         "date_added": date_added.isoformat(),
+            #         "explicit": song['track']['explicit']
+            #     })
+
+            artists_data[artist_name]["liked_songs_count"] += 1
+            
+            if date_added > artists_data[artist_name]['last_added']:
+                artists_data[artist_name]['last_added'] = date_added
+                artists_data[artist_name]['last_song'] = song_name
+                artists_data[artist_name]['last_album'] = album_name
+                artists_data[artist_name]['last_album_type'] = album_type
+
+            if date_added < artists_data[artist_name]['first_added']:
+                artists_data[artist_name]['first_added'] = date_added
+                artists_data[artist_name]['first_song'] = song_name
+                artists_data[artist_name]['first_album'] = album_name
+                artists_data[artist_name]['first_album_type'] = album_type 
+
+
+def artists_data_dictionary(artist_name: str, artist_id: str, first_added: datetime, first_song: str, 
+                            first_album: str, first_album_type: str,last_added: datetime, last_song: str, 
+                            last_album: str, last_album_type: str, num_songs_main: int, num_songs_feature: int, 
+                            liked_songs: int,genres_count: int):
     return {
         "artist_name": artist_name,
         "artist_id" : artist_id, 
@@ -133,46 +222,8 @@ def artists_data_dictionary(artist_name: str, artist_id: str, first_added: datet
         "liked_songs_count" : liked_songs,
         "genres_count": genres_count}
 
-def get_song_data(sp: spotipy.Spotify, artists_data: dict, song: dict):
-
-    artists = song['track']['artists']
-    date_added = datetime.fromisoformat(song['added_at'])
-    song_name = song['track']['name']
-    album_name = song['track']['album']['name']
-    album_type = song['track']['album']['album_type']
-
-    for artist in artists:
-        artist_name = artist['name']
-        artist_id = artist['id']
-
-        if artist_name not in artists_data:
-            genres = sp.artist(artist_id)['genres']
-            genres_count = len(genres)
-            artists_data[artist_name] = artists_data_dictionary(artist_name, artist_id, 
-                                                                     date_added, song_name, album_name, album_type,
-                                                                     date_added, song_name, album_name, album_type, 0,0,0, genres_count)
-        
-        if artist_name == artists[0]['name']:
-            artists_data[artist_name]["main_songs_count"] += 1
-        else:
-            artists_data[artist_name]["featured_songs_count"] += 1
-
-        artists_data[artist_name]["liked_songs_count"] += 1
-        
-        # keeps track of the earliest and latest songs liked by an artist 
-        if date_added > artists_data[artist_name]['last_added']:
-            artists_data[artist_name]['last_added'] = date_added
-            artists_data[artist_name]['last_song'] = song_name
-            artists_data[artist_name]['last_album'] = album_name
-            artists_data[artist_name]['last_album_type'] = album_type
-
-        if date_added < artists_data[artist_name]['first_added']:
-            artists_data[artist_name]['first_added'] = date_added
-            artists_data[artist_name]['first_song'] = song_name
-            artists_data[artist_name]['first_album'] = album_name
-            artists_data[artist_name]['first_album_type'] = album_type 
-
 def get_token():
+
     token_info = session.get(TOKEN_INFO, None)
     if not token_info:
         redirect(url_for("login", _external=False))
@@ -189,7 +240,7 @@ def get_token():
     
     return token_info
 
-def get_spotify_username(sp: spotipy.Spotify) -> str:
+def get_spotify_username(sp: spotipy.Spotify):
 
     try:
         user_info = sp.current_user()
@@ -200,13 +251,14 @@ def get_spotify_username(sp: spotipy.Spotify) -> str:
         print(f"Error getting Spotify username: {e}")
         return None
 
-
 def create_spotify_oauth():
-    return SpotifyOAuth(client_id = 'YOUR_CLIENT_ID',
-                        client_secret = 'YOUR_CLIENT_SECRET',
+    return SpotifyOAuth(client_id = "c453297c655645b9a6deac19b000ac81",
+                        client_secret = "370a6b35d2c8443eb2dbe324af8d1291",
                         redirect_uri = url_for('redirect_page', _external=True),
-                        scope = 'user-library-read playlist-modify-public playlist-modify-private user-read-recently-played playlist-read-private user-top-read')
+                        scope = 'user-library-read playlist-modify-public playlist-modify-private user-read-recently-played playlist-read-private user-top-read',
+                        requests_timeout=10)
 
 if __name__ == '__main__':
     spotifyApp.run(debug=True, port=5000)
+
 
